4ed22baf62b8c0ca27dbaa1e298390b45d52a342
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-11-15 22:19:55 +0100

    Create .R

---
+++ .R
@@ -0,0 +1,12 @@
+
+# 1.Create a map widget by leaflet().
+# 2.Adding features (e.g. addTiles, addMarkers,  addPolygons) to get the output. 
+# 3.Print the map 
+
+> library(leaflet)
+> 
+> m <- leaflet()
+> m <- addTiles(m)
+> m <- addMarkers(m, lng=174.768, lat=-36.852)
+> m #Print
+
